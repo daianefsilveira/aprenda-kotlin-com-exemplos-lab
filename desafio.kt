f78f7cebdd7268899dd655c067428874cc7e8418
daia_ <daiane.pguni@gmail.com>
2023-01-10 20:00:09 -0300

    Update desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,41 @@
+import java.sql.DriverManager.println
+
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: Nivel) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun listarInscritos(): MutableList<Usuario> {
+        return inscritos
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val conteudos: List<ConteudoEducacional> = listOf(
+        ConteudoEducacional("Linguagem Kotlin", 40),
+        ConteudoEducacional("Abstraindo Formações da DIO Usando Orientação a Objetos com Kotlin", 40))
+
+    val formacao = Formacao("Kotlin Experience", conteudos, Nivel.BASICO)
+
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+
+    println("Usuários: ${usuario1.nome}, ${usuario2.nome}")
+    println("Formacao: ${formacao.toString()}")
+    println("Inscritos na formação: ${formacao.listarInscritos()}")
 }
